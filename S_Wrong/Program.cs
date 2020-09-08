1f2cb339507984c4b68df397ced8450d33f0cffc
Guilherme Murari <guilherme.murari@moondu.io>
2020-09-08 12:59:52 +0200

    Initial commit

---
+++ S_Wrong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S_Wrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
